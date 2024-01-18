6090cb8a4f021303cf82729f2c262a9c1572e8b8
Kareemkhan34 <156427729+Kareemkhan34@users.noreply.github.com>
2024-01-19 00:02:35 +0500

    Add files via upload

---
+++ abdul_karim_midterm.py
@@ -0,0 +1,32 @@
+# Abdul Kareem
+
+
+# Q1 - Write a Python program to do arithmetical operations addition and division.
+num_1=5
+num_2=6
+sum=num_1+num_2
+print(sum)
+
+division=num_1/num_2
+print(division)
+
+# Q2 - Write a Python program to find the area of a triangle
+base=5
+height=6
+area_of_triangle =1/2*base*height
+print(area_of_triangle)
+
+# Q3 - Write a Python program to convert Celsius to Fahrenheit.
+celsius=100
+fahrenheit=celsius*9/5+32
+print(fahrenheit)
+
+# Q4 - Write a Python program that return all datatypes that we discussed in the class.
+integer=78
+print("the data Type of integer is of type",type(integer))
+float_=78.9
+print("the data Type of integer is of type",type(float))
+string="abdul_karim"
+print("the data Type of integer is of type",type(string))
+boolean=True
+print("the data Type of integer is of type",type(boolean))
